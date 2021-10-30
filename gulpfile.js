06f60ca180ae878c4dad4d3c02043688ae04d89f
Lukas Weixelbraun <mail@lukas-weixelbraun.com>
2021-10-30 02:10:48 +0200

    removed navbar css to own repo

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,18 @@ var cssnano = require('cssnano');
 var vhfix = require('postcss-100vh-fix');
 
 gulp.task('css', function() {
-    var plugin = [
-        autoprefixer(),
-        cssnano(),
-        vhfix()
-    ];
-    return gulp.src('./css/*.css')
-            .pipe(postcss(plugin))
-            .pipe(gulp.dest('./public'));
+  var plugin = [
+    autoprefixer(),
+    cssnano(),
+    vhfix()
+  ];
+  return gulp.src('./css/*.css')
+    .pipe(postcss(plugin))
+    .pipe(gulp.dest('./public'));
 });
 
 gulp.task('serve', gulp.series('css'), function() {
-    gulp.watch("./css/*.css", ['css']);
+  gulp.watch("./css/*.css", ['css']);
 });
 
 gulp.task('default', gulp.series('serve'));
