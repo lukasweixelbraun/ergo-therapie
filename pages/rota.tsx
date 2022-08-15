d648337138c6a9c3b96513a2928cd2d9db02ba1c
Lukas Weixelbraun <mail@lukas-weixelbraun.com>
2022-08-15 12:00:56 +0200

    changed rota text

--- pages/rota.tsx
+++ pages/rota.tsx
@@ -26,15 +26,52 @@ const Rota: NextPage = () => {
 
         <h3 className="heading mt-5">Für wen ist diese Therapieform?</h3>
         <p>
-          Durch Körperübungen, die eine Rotation der Wirbelsäule, als auch Rotation des Körpers um seine Achsen im Raum
-          beinhalten, wird das Gehirn angeregt, die Körperspannung optimal zu regulieren. Verspannungen, die durch
-          verborgen noch aktive Restreflexe aus der frühen Kindheit entstehen, werden gelöst. Impulse zur Verbesserung
-          der Mundmotorik und Wahrnehmung unterstützen wesentlich die positive und ausgleichende Wirkung für eine gute
-          sensomotorische Entwicklung. Die Arbeits-, Schul- und Freizeitgestaltung wird so angepasst, dass sich
-          vorhandene Symptome, Konzentrationsschwächen oder Beeinträchtigungen verbessern. Im motorischen als auch
-          vegetativen System: Essen, Schlafen, Verdauung.
-          (Rota Therapie, 2021)
+          Die Rota-Therapie kann grundsätzlich in jedem Alter und bei unterschiedlichsten Belastungen geeignet sein. 
+          Mit einem individuellen Übungsprogramm wird das Vorgehen in einzelnen Sitzungen angeleitet und die Alltagsgestaltung 
+          besprochen und adaptiert. Daher können Eltern, Angehörige oder die PatientInnen selbst die Prinzipien der 
+          Rota-Therapie erlernen und diese täglich anwenden. Zusätzlich ist es perfekt als Prophylaxe geeignet. 
         </p>
+
+        <p>
+          Bei Säuglingen und kleinen Kindern werden die Übungen am Schoß ausgeführt, größere Kinder und Erwachsene üben 
+          auf dem Boden oder im Bett. Eine Rota-Position eignet sich sogar als Schlafposition und wirkt sich 
+          positiv auf unser Zentralnervensystem aus.
+        </p>
+
+        <p>
+          Die Gemeinsamkeit der Probleme ist die Tonusregulation – darauf kann die Rota-Therapie guten Einfluss nehmen. 
+          Bei vielen Krankheitsbildern ist die zentral gesteuerte Basis der Körperspannung nicht optimal reguliert. 
+        </p>
+
+        <h3 className="heading mt-5">Mögliche Einsatzgebiete der Rota-Therapie</h3>
+        <ul>
+          <li>Zentrale Koordinationsstörung</li>
+          <li>Frühgeborene</li>
+          <li>Risikokinder</li>
+          <li>Schreibabys</li>
+          <li>Saug- und Stillprobleme</li>
+          <li>Entwicklungsverzögerung</li>
+          <li>Genetische Besonderheiten
+            <ul>
+              <li>Down Syndrom</li>
+            </ul>
+          </li>
+          
+          <li>Autismus</li>
+          <li>Wahrnehmungsstörungen</li>
+          <li>Konzentrations- und Lernstörungen</li>
+          <li>AD(H)S</li>
+          <li>Skoliose und Wirbelsäulenbeschwerden</li>
+          <li>Hüftdysplasien und -luxationen</li>
+          <li>Fußfehlstellungen</li>
+          <li>Neurologische Krankheitsbilder
+            <ul>
+              <li>Parkinson</li>
+              <li>MSSchlaganfall</li>
+            </ul>
+          </li>
+          <li>Spastische Bedrohung und/oder Behinderung</li>
+        </ul>
 
         <h3 className="heading mt-5">Die Besonderheiten</h3>
         <ul>
